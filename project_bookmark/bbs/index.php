f728720455cf9fa44ca8cec08c3268463e14e508
limsubin <ddh053@naver.com>
2019-11-03 02:04:52 +0900

    project_boomark
    project_boomark

--- project_bookmark/bbs/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-  require_once("db.php");
-  $sql = 'SELECT * FROM myboards ORDER BY id DESC';
-  $statement = $connection->prepare($sql);
-  $statement->execute();
-  $boards = $statement->fetchAll(PDO::FETCH_OBJ);
-?>
--- project_bookmark/member/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once("../tools.php");
-
-    session_start();
-    session_destroy();
-
-    goNow(MAIN_PAGE);
-?>
